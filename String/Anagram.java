bb98507ec0d998e3bbb0bb922323ab4c6604c961
sanket <sanket.de333@gmail.com>
2023-02-22 12:22:10 +0530

    Anagram Program

---
+++ String/Anagram.java
@@ -0,0 +1,23 @@
+package String;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String s1 = "silent";
+        String s2 = "listen";
+
+        System.out.println(isAnagram(s1,s2));
+    }
+    public static boolean isAnagram(String s1 , String s2)
+    {
+       while(true)
+       {
+
+           if(s1.length() != s2.length()) return false;
+           if(s1.length() == 0 && s2.length() == 0) return true;
+           char ch = s1.charAt(0);
+           s1 = s1.replace(ch+"","");
+           s2 = s2.replace(ch+"","");
+       }
+
+    }
+}
