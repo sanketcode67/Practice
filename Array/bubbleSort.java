c7009a7d494a0d88cf8f81706d36ec32b9147e22
sanket <sanket.de333@gmail.com>
2023-03-03 20:18:27 +0530

    Bubble Sort

---
+++ Array/bubbleSort.java
@@ -0,0 +1,31 @@
+package Array;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class bubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr)
+    {
+        int n = arr.length;
+
+        for(int i = 0; i < n-1; i++)
+        {
+            for(int j = 0; j < n-1-i; j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
